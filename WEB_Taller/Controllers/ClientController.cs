9d30a4d94e770142c1613dc0f2bfd50e41f8d1f8
Rendery Codes <48937882+Rendery@users.noreply.github.com>
2023-08-24 16:33:35 -0500

    Get Clientes Modelo Controlador

---
+++ WEB_Taller/Controllers/ClientController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_Taller.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WEB_Taller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        // GET: api/<ClientController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ClientController>/5
+        [HttpGet("{id}")]
+        public Client Get(int id)
+        {
+            Client client = new Client();
+            client.Id = id;
+            client.Name = "Rendery";
+            client.Description = """Frencuente 24/7""";
+            return client;
+        }
+
+        // POST api/<ClientController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/<ClientController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
