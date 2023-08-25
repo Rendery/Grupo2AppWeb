651d009156d9ad788474d0bd3a47fc150f496372
Mariano Vilela Menéndez <89040378+marianovilela@users.noreply.github.com>
2023-08-25 11:43:05 -0500

    ComponentsController - Delete

---
+++ Grupo2AppWeb-main/WEB_Taller/Model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_Taller.Model
+{
+    public class Category
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public int Count { get; set; }
+    }
+}
