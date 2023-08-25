ffcd46c01a75f90cd0718c947086c1c7b9e33369
FabrizioTorres <89040324+FabrizioTorres@users.noreply.github.com>
2023-08-24 20:54:46 -0500

    Components
    Se creo el modelo y controloador components , agregandole id,nombre, description y precio.

---
+++ WEB_Taller/Controllers/ComponentsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_Taller.Model;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WEB_Taller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComponentsController : ControllerBase
+    {
+        // GET: api/<ComponentsController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ComponentsController>/5
+        [HttpGet("{id}")]
+        public Components Get(int id)
+        {
+            Components componente=new Components();
+            componente.Id = id;
+            componente.Name = "name";
+            componente.Description = "description";
+            componente.Price = 8;
+            return componente;
+        }
+
+        // POST api/<ComponentsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ComponentsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ComponentsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WEB_Taller/Model/Components.cs
@@ -0,0 +1,10 @@
+﻿namespace WEB_Taller.Model
+{
+    public class Components
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+}
